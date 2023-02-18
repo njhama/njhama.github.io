3888ab7cb991a13c69c1cf078040df90de6c6ae6
Nicholas Hama <njhama@usc.edu>
2023-02-18 15:52:49 -0800

    fix

--- main.js
+++ main.js
@@ -17,7 +17,4 @@ if (!languageSelect) {console.error('The language select element could not be fo
 }
 languageSelect.addEventListener('change', function()  {window.location.href = window.location.host.includes("translate.goog") ? window.location.host = "https://njhama.github.io" + window.location.pathname : "https://njhama-github-io.translate.goog" + window.location.pathname + "?_x_tr_sl=auto&_x_tr_tl=" + languageCodes[this.value] + "&_x_tr_hl=en&_x_tr_pto=wapp";})
 
-//languageSelect.addEventListener('change', function()  {window.location.href = window.location.host.includes("translate.goog") ? window.location.host = "https://njhama.github.io" + window.location.pathname : "https://njhama-github-io.translate.goog" + window.location.pathname + "?_x_tr_sl=auto&_x_tr_tl=" + languageCodes[this.value] + "&_x_tr_hl=en&_x_tr_pto=wapp";});
-
-//https://www-njhama-github-io.translate.goog/subpage.html?_x_tr_sl=auto&_x_tr_tl=it&_x_tr_hl=en&_x_tr_pto=wapp
-//https://njhama-github-io.translate.goog/subpage.html?_x_tr_sl=auto&_x_tr_tl=it&_x_tr_hl=en&_x_tr_pto=wapp
+//languageSelect.addEventListener('change', function()  {window.location.href = window.location.host.includes("translate.goog") ? window.location.host = "https://uscannenbergmedia-com" + window.location.pathname : "https://uscannenbergmedia-com.translate.goog" + window.location.pathname + "?_x_tr_sl=auto&_x_tr_tl=" + languageCodes[this.value] + "&_x_tr_hl=en&_x_tr_pto=wapp";})
