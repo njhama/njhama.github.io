9e5c26cf8e8328ad85c3494791367839b25d2856
Nicholas Hama <njhama@usc.edu>
2023-02-17 17:27:31 -0800

    fix

--- main.js
+++ main.js
@@ -4,11 +4,7 @@ const languageCodes = {
   "spanish": "es",
   "italian": "it"
 };
-
-
-
 const languageSelect = document.getElementById('dropdown');
-
 if (!languageSelect) {
   console.error('The language select element could not be found');
 } else {
@@ -19,22 +15,24 @@ if (!languageSelect) {
     languageSelect.appendChild(option);
   });
 }
-
 var currLang = "english";
-
-
 languageSelect.addEventListener('change', function() {
-  url = window.location.href;
   const selectedLanguage = this.value;
+  
   const languageCode = languageCodes[selectedLanguage];
+  console.log(`Selected language: ${selectedLanguage} (${languageCode})`);
+  url = window.location.href;
+  console.log("url " + url)
+  //get the dir
   let web_ending = ".io"
   const indexOfCom = url.indexOf(web_ending);
+  console.log(indexOfCom)
+  console.log(web_ending.length)
   let page = url.substring(0,indexOfCom + web_ending.length)
   page = page.replace(/\./g, "-") + ".translate.goog";
   const dir = url.substring(indexOfCom + web_ending.length);
-  
-  let newUrl =  page + "?_x_tr_sl=auto&_x_tr_tl=" + languageCode + "&_x_tr_hl=en&_x_tr_pto=wapp";
-  console.log(newUrl)
-  window.location.href = newUrl
-  
+ 
+ let newUrl =  "https://" + page + "?_x_tr_sl=auto" + "&_x_tr_tl=" + languageCode  + "&_x_tr_hl=en&_x_tr_pto=wapp"
+  currLang = selectedLanguage;
+  window.location.href = newUrl;
 });
