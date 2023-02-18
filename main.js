d70bdf2f2a3012c050e35fa0696019c45e2c7549
Nicholas Hama <njhama@usc.edu>
2023-02-18 14:53:50 -0800

    fix

--- main.js
+++ main.js
@@ -19,6 +19,12 @@ if (!languageSelect) {console.error('The language select element could not be fo
 languageSelect.addEventListener('change', function() {
   let newUrl;
   if (window.location.host.includes("translate.goog"))  {newUrl= window.location.host = "www.njhama.github.io" + window.location.pathname;}
-  else  {newUrl = "http://njhama-github-io.translate.goog"+window.location.pathname+"?_x_tr_sl=auto&_x_tr_tl="+languageCodes[this.value]+"&_x_tr_hl=en&_x_tr_pto=wapp";} 
+  else  {newUrl = "https://njhama-github-io.translate.goog"+window.location.pathname+"?_x_tr_sl=auto&_x_tr_tl="+languageCodes[this.value]+"&_x_tr_hl=en&_x_tr_pto=wapp";} 
   window.location.href = newUrl;
 });
+
+
+//languageSelect.addEventListener('change', function()  {window.location.href = window.location.host.includes("translate.goog") ? window.location.host = "www.njhama.github.io" + window.location.pathname : "http://njhama-github-io.translate.goog" + window.location.pathname + "?_x_tr_sl=auto&_x_tr_tl=" + languageCodes[this.value] + "&_x_tr_hl=en&_x_tr_pto=wapp";});
+
+//https://www-njhama-github-io.translate.goog/subpage.html?_x_tr_sl=auto&_x_tr_tl=it&_x_tr_hl=en&_x_tr_pto=wapp
+//https://njhama-github-io.translate.goog/subpage.html?_x_tr_sl=auto&_x_tr_tl=it&_x_tr_hl=en&_x_tr_pto=wapp
