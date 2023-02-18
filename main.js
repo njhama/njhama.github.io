6996d02ebc7bfabbde7d68cefc1f1fe0deb6a335
Nicholas Hama <njhama@usc.edu>
2023-02-18 14:21:26 -0800

    fix

--- main.js
+++ main.js
@@ -21,5 +21,4 @@ if (!languageSelect) {
 languageSelect.addEventListener('change', function() {
   if (window.location.host.includes("translate.goog"))  {window.location.host = window.location.host = "www.uscannenbergmedia.com" + window.location.pathname;}
   else  {window.location.host = "www-njhama-github-io.translate.goog"+window.location.pathname+"?_x_tr_sl=auto&_x_tr_tl="+languageCodes[this.value]+"&_x_tr_hl=en&_x_tr_pto=wapp";} 
-
 });
