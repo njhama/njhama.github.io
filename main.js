8f4c67475006d278a656cd7ff2f23f1b46ed1b69
Nicholas Hama <njhama@usc.edu>
2023-02-18 14:13:56 -0800

    fix

--- main.js
+++ main.js
@@ -19,8 +19,8 @@ if (!languageSelect) {
 }
 
 languageSelect.addEventListener('change', function() {
-  if (window.location.host.includes("translate.goog"))  {alert(window.location.host = "www.uscannenbergmedia.com" + window.location.pathname);}
-  else  {alert("www-uscannenbergmedia-com.translate.goog"+window.location.pathname+"?_x_tr_sl=auto&_x_tr_tl="+languageCodes[this.value]+"&_x_tr_hl=en&_x_tr_pto=wapp");} 
+  if (window.location.host.includes("translate.goog"))  {window.location.host = window.location.host = "www.uscannenbergmedia.com" + window.location.pathname;}
+  else  {window.location.host = "www-uscannenbergmedia-com.translate.goog"+window.location.pathname+"?_x_tr_sl=auto&_x_tr_tl="+languageCodes[this.value]+"&_x_tr_hl=en&_x_tr_pto=wapp";} 
 
 });
 //if (window.location.host.includes("translate.goog"))  {window.location.host = "www.uscannenbergmedia.com" + window.location.pathname;}
