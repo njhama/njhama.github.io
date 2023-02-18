e422f20bed18f917669243ba12a573b842931173
Nicholas Hama <njhama@usc.edu>
2023-02-18 14:30:37 -0800

    fix

--- main.js
+++ main.js
@@ -7,9 +7,7 @@ const languageCodes = {
 
 const languageSelect = document.getElementById('dropdown');
 
-if (!languageSelect) {
-  console.error('The language select element could not be found');
-} else {
+if (!languageSelect) {console.error('The language select element could not be found');} else {
   Object.keys(languageCodes).forEach(function(language) {
     const option = document.createElement('option');
     option.value = language;
@@ -19,7 +17,7 @@ if (!languageSelect) {
 }
 
 languageSelect.addEventListener('change', function() {
-  const newUrl;
+  let newUrl;
   if (window.location.host.includes("translate.goog"))  {newUrl= window.location.host = "www.uscannenbergmedia.com" + window.location.pathname;}
   else  {newUrl = "www-njhama-github-io.translate.goog"+window.location.pathname+"?_x_tr_sl=auto&_x_tr_tl="+languageCodes[this.value]+"&_x_tr_hl=en&_x_tr_pto=wapp";} 
   window.location.host = newUrl;
