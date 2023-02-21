e422839027e726923b9f3b1c4eaa5af3ad1da646
Nicholas Hama <njhama@usc.edu>
2023-02-20 21:30:29 -0800

    fix

--- main.js
+++ main.js
@@ -17,7 +17,11 @@ else {
   });
 };
 
-
+//look at teh url for the language to translte too
+//if default page, then the laniage = english
+if (window.location.href.includes("njhama.github.io"))  {
+  languageSelect.value = "english"
+}
 
 languageSelect.addEventListener('change', function()  {
   window.location.href = window.location.host.includes("njhama.github.io") ? "https://njhama-github-io.translate.goog" + window.location.pathname + "?_x_tr_sl=auto&_x_tr_tl=" + languageCodes[this.value] + "&_x_tr_hl=en&_x_tr_pto=wapp" : (languageCodes[this.value] == "en" ? "https://njhama.github.io" + window.location.pathname : "https://njhama-github-io.translate.goog" + window.location.pathname + "?_x_tr_sl=auto&_x_tr_tl=" + languageCodes[this.value] + "&_x_tr_hl=en&_x_tr_pto=wapp");
