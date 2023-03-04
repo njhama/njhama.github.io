0ca1c73513b4a0c51d6cd248b195696a85dd73d0
Nicky Hama <nickyhama@PC.usc.edu>
2023-03-03 16:33:27 -0800

    checking func

--- main.js
+++ main.js
@@ -14,17 +14,32 @@ const languageCodes = new Map([
   ["Portuguese", "pt"],
 ]);
 
-const languageSelect = document.getElementById('dropdown');
+//iterate through the keys and create new li elements change the inner hhtl to hte key and then append the li to the ul
 
-if (!languageSelect) {
-  console.error('The language select element could not be found');
-} 
+
+//old dorp don
+const languageSelect = document.getElementById('dropdown');
+const myList = document.querySelector('#dropdown-menu');
+if (!languageSelect) {console.error('The language select element could not be found');} 
 else {
   languageCodes.forEach(function(value, key) {
-    const option = document.createElement('option');
-    option.value = key;
-    option.textContent = key;
-    languageSelect.appendChild(option);
+    //old
+    //const option = document.createElement('option');
+    //option.value = key;
+    //option.textContent = key;
+    //languageSelect.appendChild(option);
+
+    //new
+    //create a new list
+    //create a new A with the innerHTML of the key
+    //add it to myLIst
+
+    const newList = document.createElement('li');
+    const newLink = document.createElement('a');
+    newLink.classList.add("dropdown-item");
+    newLink.innerHTML = key;
+    newList.appendChild(newLink);
+    myList.appendChild(newList);
   });
 };
 
@@ -38,13 +53,35 @@ else  {
   let languageCode = window.location.href.substring(window.location.href.indexOf("_x_tr_tl=") + 9).split("&")[0];
   for (const [key, value] of languageCodes.entries()) {
     if (value === languageCode) {
-      languageSelect.value = key;
+      //languageSelect.value = key;
+      //document.querySelector('#translateButton').innerHTML = key;
+      console.log("new language", key);
+      //document.getElementById
       break;
     }
   }
 
-  alert("Disclaimer: translations are automated and may not be completely accurate");
+  //alert("Disclaimer: translations are automated and may not be completely accurate");
   
+
+
+  
+ 
+
+myList.addEventListener('click', function(event) {
+  console.log('click');
+  if (event.target.tagName === 'A') {
+    
+    // Call your function here, passing the clicked element if needed
+    myFunction(event.target);
+  }
+});
+
+function myFunction(clickedElement) {
+  console.log('The following li element was clicked:', clickedElement.innerHTML);
+  window.location.href = window.location.host.includes("njhama.github.io") ? "https://njhama-github-io.translate.goog" + window.location.pathname + "?_x_tr_sl=auto&_x_tr_tl=" + languageCodes.get(clickedElement.innerHTML) + "&_x_tr_hl=en&_x_tr_pto=wapp" : (languageCodes.get(clickedElement.innerHTML) == "en" ? "https://njhama.github.io" + window.location.pathname : "https://njhama-github-io.translate.goog" + window.location.pathname + "?_x_tr_sl=auto&_x_tr_tl=" + languageCodes.get(clickedElement.innerHTML) + "&_x_tr_hl=en&_x_tr_pto=wapp");
+  // Do something else with the clicked element
+}
 }
 
 
